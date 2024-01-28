ad053f176236345e47ff96a5a6b222541b513270
sotjim <122051655+sotjim@users.noreply.github.com>
2024-01-28 15:22:25 +0200

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,19 @@ input.onButtonPressed(Button.A, function () {
     wuKong.setMotorSpeed(wuKong.MotorList.M2, -50)
     wuKong.setLightMode(wuKong.LightMode.BREATH)
 })
+input.onSound(DetectedSound.Loud, function () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 100)
+    basic.pause(5000)
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 90)
+})
 input.onButtonPressed(Button.B, function () {
     wuKong.setMotorSpeed(wuKong.MotorList.M1, 50)
     wuKong.setMotorSpeed(wuKong.MotorList.M2, 50)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 120)
-    basic.pause(2000)
-    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 100)
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 80)
+    basic.pause(5000)
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 90)
 })
 basic.showIcon(IconNames.Heart)
 let rainwater = false
@@ -24,6 +29,7 @@ basic.forever(function () {
     if (Environment.ReadWaterLevel(AnalogPin.P1) < 10) {
         if (rainwater == true) {
             rainwater = false
+            wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 90)
         }
     }
 })
