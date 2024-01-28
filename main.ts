14e2409ea7f4324333783888719883ecf980ef3c
sotjim <122051655+sotjim@users.noreply.github.com>
2024-01-28 14:39:32 +0200

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, -66)
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, -50)
+})
+input.onButtonPressed(Button.B, function () {
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, 50)
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, 50)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 120)
+    basic.pause(2000)
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 100)
+})
+basic.showIcon(IconNames.Heart)
+let rainwater = false
+basic.forever(function () {
+    if (Environment.ReadWaterLevel(AnalogPin.P1) > 10) {
+        if (rainwater == false) {
+            wuKong.stopAllMotor()
+            rainwater = true
+        }
+    }
+    if (Environment.ReadWaterLevel(AnalogPin.P1) < 10) {
+        if (rainwater == true) {
+            rainwater = false
+        }
+    }
+})
